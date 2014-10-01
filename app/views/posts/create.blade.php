093e985587987860137fb8553fa8afb6798c35dd
dannywng <danielwng@126.com>
2014-10-01 21:16:59 +0800

    post

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -8,14 +8,15 @@
 <body style="margin:30px;">
 	<h1>上传图片</h1>
 
-    <form action="http://v0.api.upyun.com/mypicwebsite" 
+	<input type="file" name="file" multiple>
+	<span id="formdataSubmit">点击我上传</span>
+
+    <form id="uploadForm" action="http://v0.api.upyun.com/mypicwebsite" 
 	method="post" enctype="multipart/form-data">
 
       <!-- 需要传递以下三个表单内容 -->
       <input type="hidden" name="policy" value="">
       <input type="hidden" name="signature" value="">
-      <input type="file" name="file">
-      <input type="submit" value="上传">
 			
     </form>
     <script src="/components/vendor/jquery/dist/jquery.min.js"></script>
--- public/js/upyunUpload.js
+++ public/js/upyunUpload.js
@@ -1,8 +1,73 @@
 jQuery(function($){
 
-	$.getJSON("/handlers/upyun/formAPI_getSignature.php",function(data){
+	upYun = {
 
-		alert(data);
+		setToken : function(getUrl){
+
+			$.getJSON(getUrl,function(data){
+
+				$('input[name=policy]').val(data.policy);
+
+				$('input[name=signature]').val(data.signature);
+
+			});
+
+		},
+
+		createFormData : function(formElement){
+
+			return new FormData(formElement);
+
+		},
+
+		getUploadFiles : function(fileInputElement){
+
+			return fileInputElement.files;
+
+		}
+
+	};
+
+
+	upYun.setToken("/handlers/upyun/formAPI_getToken.php");
+
+	var form = $('uploadForm')[0];
+
+	var formData = upYun.createFormData(form);
+
+	var fileInput = $('input[name=file]')[0];
+
+
+
+
+
+
+
+
+
+
+
+
+	$("#formdataSubmit").click(function(event){
+
+		var files =upYun.getUploadFiles(fileInput);
+
+		formData.append("file",files[0]);
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST","http://v0.api.upyun.com/mypicwebsite",true);
+		xhr.onreadystatechange = function(){
+
+			if(xhr.readyState==4 && xhr.status==200){
+
+				alert(xhr.responseText);
+
+			}
+		}
+
+		xhr.send(formData);
+
+
 
 	});
 
